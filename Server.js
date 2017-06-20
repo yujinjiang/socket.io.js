1fc09523b34dad59d1bf75dc80b911842ac5c52f
yujinjiang <2879032410@qq.com>
2017-06-20 23:40:40 +0800

    ssl

---
+++ Server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+const fs = require('fs');
+const server = http.createServer(function(req, res) {
+    fs.readFile(__dirname + '/client.html', function(err, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.writeHead(200, { 'Context-type': 'text/plain' });
+            res.end(data);
+        };
+    });
+});
+server.listen(8080, function() {
+    console.log('ok 8080');
+});
+const io = require('socket.io')(server);
+io.on('connection', function(socket) {
+    socket.emit('news', { wwe: 'wwe' });
+    socket.on('mymes', function(data) {
+        console.log(data);
+    })
+});
---
+++ socket.js
@@ -0,0 +1,17 @@
+var io = require('socket.io')(80);
+io.on('connection', (socket) => {
+    console.log('ok connection ');
+    console.log(socket.id);
+    socket.write(123);
+    socket.on('private message', (data) => {
+        console.log(data, `private`);
+    });
+    socket.on('disconnect', () => {
+        console.log('out here');
+    });
+    socket.on('message', (data) => {
+        console.log(`${data}`, 'message');
+    });
+    socket.emit('socket connection', `${socket.id}`);
+    io.sockets.emit('socket connection', `data`);
+});
