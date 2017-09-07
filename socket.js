9e17a5b2117d7acfa2439078b86585a660c512ea
yujinjiang <2879032410@qq.com>
2017-09-08 00:14:52 +0800

    moni

--- socket.js
+++ socket.js
@@ -1,6 +1,6 @@
 var io = require('socket.io')(80);
 var get = 'dds';
- get ='bash';
+var gets ='bash';
 io.on('connection', (socket) => {
     console.log('ok connection ');
     console.log(socket.id);
